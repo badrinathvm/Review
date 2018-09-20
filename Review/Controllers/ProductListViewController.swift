40a751234479b0a0f06e02dc5a77863f0d42fefd
Badarinath Venkatnarayansetty <bvenkatnarayansetty@evernote.com>
2018-09-20 15:30:51 -0700

    Removed warnings

--- Review/Controllers/ProductListViewController.swift
+++ Review/Controllers/ProductListViewController.swift
@@ -12,19 +12,19 @@ class ProductListViewController: UIViewController {
 
     fileprivate let cellIdentifier = "productCell"
 
-    var productData:[Product] = []
-    
+    var productData: [Product] = []
+
     lazy var productModal: ProductModal = { [unowned self] in
-       let modal = ProductModal()
-       modal.delegate = self
-       return modal
+        let modal = ProductModal()
+        modal.delegate = self
+        return modal
     }()
-    
+
     lazy var filterOrDisable: UIBarButtonItem = { [unowned self] in
         let barButton = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterTapped))
         return barButton
     }()
-    
+
     lazy var tableView: UITableView = { [unowned self] in
         let tableView = UITableView()
         tableView.register(ProductTableViewCell.self, forCellReuseIdentifier: cellIdentifier)
@@ -33,50 +33,50 @@ class ProductListViewController: UIViewController {
         tableView.delegate = self
         return tableView
     }()
-    
+
     lazy var persistenceHelper: PersistenceHelper = { [unowned self] in
         let persistenceHelper = PersistenceHelper()
         persistenceHelper.delegate = self
         return persistenceHelper
     }()
-    
-    
+
+
     override func viewDidLoad() {
-        
+
         super.viewDidLoad()
-        
+
         setupTableView()
-        
+
         setupNavItems()
 
         productModal.fetchProductData()
     }
 }
 
-extension ProductListViewController : UITableViewDelegate, UITableViewDataSource {
-    
+extension ProductListViewController: UITableViewDelegate, UITableViewDataSource {
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return productModal.productData.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = ProductTableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: cellIdentifier)
         cell.delegate = self
-        
-       let productViewModel = ProductViewModel(product: productModal.productData[indexPath.row])
-       productViewModel.configure(cell)
-        
+
+        let productViewModel = ProductViewModel(product: productModal.productData[indexPath.row])
+        productViewModel.configure(cell)
+
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 90
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("Selected Row is \(indexPath.row)")
-        tableView.deselectRow(at: indexPath, animated:  true)
+        tableView.deselectRow(at: indexPath, animated: true)
         let product = productData[indexPath.row]
         navigateToProductDetails(with: product)
     }
@@ -85,14 +85,14 @@ extension ProductListViewController : UITableViewDelegate, UITableViewDataSource
 //MARK : NSUserDefaults updates and fav img updates.
 
 extension ProductListViewController: PersistenceDelegate {
-    
+
     //updates the product list
     func updateCache(productList: [Product]) {
         persistenceHelper.updateUserDefaults(for: productList)
     }
-    
+
     //Return the list for products.
-    func getProductList ()  -> [Product] {
+    func getProductList () -> [Product] {
         return persistenceHelper.fetchFromUserDefaults()
     }
 }
@@ -101,17 +101,17 @@ extension ProductListViewController: PersistenceDelegate {
 extension ProductListViewController: ProductTableViewCellDelegate {
 
     func didTapFavorite(_ sender: ProductTableViewCell) {
-        
+
         guard let tappedIndexPath = tableView.indexPath(for: sender) else { return }
         let favCell = self.tableView.cellForRow(at: tappedIndexPath) as? ProductTableViewCell
         let productList = persistenceHelper.fetchProductsFromCache()
         let product = productList[tappedIndexPath.row]
         guard let productFav = product.fav else { return }
-        
+
         if productFav {
             favCell?.favImg.image = UIImage(named: "outline.png")
             product.fav = false
-        }else{
+        } else {
             favCell?.favImg.image = UIImage(named: "filled.png")
             product.fav = true
         }
@@ -129,31 +129,31 @@ extension ProductListViewController: ProductModalDelegate {
 //MARK : UI updates and navigations
 
 extension ProductListViewController {
-    
+
     func setupTableView() {
-        
+
         self.view.addSubview(tableView)
-        
+
         tableView.anchor(top: self.view.safeAreaLayoutGuide.topAnchor, leading: self.view.safeAreaLayoutGuide.leadingAnchor, bottom: self.view.safeAreaLayoutGuide.bottomAnchor, trailing: self.view.safeAreaLayoutGuide.trailingAnchor)
     }
-    
+
     func setupNavItems() {
         navigationItem.rightBarButtonItem = filterOrDisable
     }
-    
+
     func navigateToProductDetails(with product: Product) {
-        guard let productDetailsVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ProductDetailViewController") as? ProductDetailViewController else  { return }
+        guard let productDetailsVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ProductDetailViewController") as? ProductDetailViewController else { return }
         productDetailsVC.product = product
         self.navigationController?.navigationBar.backItem?.title = NSLocalizedString("Product Details", comment: "Product Details ")
         self.navigationController?.pushViewController(productDetailsVC, animated: true)
     }
-    
+
     @objc func filterTapped() {
         let productList = persistenceHelper.fetchProductsFromCache()
         if filterOrDisable.title == "Filter" {
             filterOrDisable.title = "Disable"
-            self.productModal.productData = productList.filter{ $0.fav == true }
-        }else {
+            self.productModal.productData = productList.filter { $0.fav == true }
+        } else {
             filterOrDisable.title = "Filter"
             self.productModal.productData = productList
         }
